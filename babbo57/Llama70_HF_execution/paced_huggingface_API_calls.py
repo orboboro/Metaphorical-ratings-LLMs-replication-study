379881b0130f3554ac0c5b2303b61841cbdee37d
orboboro <jacopo.1205@gmail.com>
2025-12-02 09:03:33 +0100

    c

--- babbo57/Llama70_HF_execution/paced_huggingface_API_calls.py
+++ babbo57/Llama70_HF_execution/paced_huggingface_API_calls.py
@@ -118,7 +118,7 @@ def huggingface_API_calling(dataset, model, raters, test = False):
             print(rater, idx + 1, "of", len(metaphors_list))
             structure = structures_list[idx]
 
-            client = InferenceClient(api_key=os.environ["HF_TOKEN_BABBO"], provider = "novita")
+            client = InferenceClient(api_key=os.environ["HF_TOKEN_BABBO"], provider = "nebius")
 
             conversation[-1]["content"][0]["text"] = metaphor
 
