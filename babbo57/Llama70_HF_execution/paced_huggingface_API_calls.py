6ff2890f75ac7a4f3a52ff47cc0093f5be99e165
orboboro <jacopo.1205@gmail.com>
2026-01-07 15:39:27 +0100

    c

--- babbo57/Llama70_HF_execution/paced_huggingface_API_calls.py
+++ babbo57/Llama70_HF_execution/paced_huggingface_API_calls.py
@@ -259,7 +259,7 @@ def huggingface_API_calling(dataset, model, raters, temperature, memory, test):
             write_out(out_annotation_file, row)
 
             minuto = 60
-            time.sleep(1 * minuto)
+            time.sleep(0.5 * minuto)
 
         print(f"{rater} rated all metaphors\n")
 
